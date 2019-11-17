d979c44f95f74eaec63d64ab4f56d05cac5482ab
Santiago Molinari <santomolinari@gmail.com>
2019-11-17 15:07:22 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -22,7 +22,7 @@ def assembled_matrix
 end
 
 def array_literal_matrix
-  array.sort = [ENNET_HOUSE,ENFIELD_TENNIS_ACADEMY]
+  return array.sort = [ENNET_HOUSE,ENFIELD_TENNIS_ACADEMY]
 
 end
     # Using Array literal syntax only, build a nested array that uses the data inside the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
